7b19f2d5c3542e7b3e5f0b36e445a0af7d40410d
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-04-13 10:27:53 +0300

    watch and computed methods

--- first_lesson/app.js
+++ first_lesson/app.js
@@ -1,16 +1,38 @@
 const App = {
   data() {
     return {
-      title: 'Список заметок',
-      placeholderString: 'Введите название',
-      inputValue: ''
-    }
+      title: "Список заметок",
+      placeholderString: "Введите название",
+      inputValue: "",
+      notes: ["заметка 1", "заметка 2"],
+    };
   },
   methods: {
     inputChangeHandler(event) {
-      this.inputValue = event.target.value
+      this.inputValue = event.target.value;
+    },
+    addNewNote() {
+      if (this.inputValue) {
+        this.notes.push(this.inputValue);
+        this.inputValue = "";
+      }
+    },
+    removeNote(i) {
+      this.notes.splice(i, 1)
+    },
+  },
+  computed: {
+    doubleCountComputed() {
+      return this.notes.length * 2
+    }
+  },
+  watch: {
+    inputValue(value) {
+      if (value.length > 10) {
+        this.inputValue = '';
+      }
     }
   }
-}
+};
 
-Vue.createApp(App).mount('#app')
+Vue.createApp(App).mount("#app");
