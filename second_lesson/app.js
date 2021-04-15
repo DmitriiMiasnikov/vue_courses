07f9ed9d29e31fadb8be6a0a7a3ef078af07fa39
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-04-15 13:33:09 +0300

    for, key

--- second_lesson/app.js
+++ second_lesson/app.js
@@ -1,7 +1,12 @@
 const App = {
   data: () => ({
     title: 'Заголовок',
-    myHtml: '<h1>vue 2 lesson</h1>'
+    myHtml: '<h1>vue 2 lesson</h1>',
+    person: {
+       firstName: 'dmitrii',
+       lastName: 'miasnikov'
+     },
+     items: [1,2,3,4,5,6]
   })
 }
 
